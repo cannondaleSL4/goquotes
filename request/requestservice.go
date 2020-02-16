22f51780186064f9e5ab11a4180e02535b501cce
Dmitriy <workday2006@yandex.ru>
2020-02-16 12:01:25 +0300

    add tinkoff client

--- request/requestservice.go
+++ request/requestservice.go
@@ -1,17 +1,20 @@
 package request
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
+	"flag"
+	"fmt"
 	quotes "github.com/goquotes/const"
+	"log"
+	"os"
+	"time"
 
-	"github.com/tidwall/gjson"
-	"io/ioutil"
-	"net/http"
+	tinkoff "github.com/TinkoffCreditSystems/invest-openapi-go-sdk"
 )
 
+var token = flag.String("token", os.Getenv("TOKEN"), "your token")
 
-func UpdateFromTo(from string, to string) []quotes.StocksFromResponse{
+func UpdateFromTo(from string, to string) []quotes.StocksFromResponse {
 	var arrayOfRequestData []quotes.RequestData
 	for _, element := range quotes.GetQuotes() {
 		var req quotes.RequestData
@@ -24,36 +27,46 @@ func UpdateFromTo(from string, to string) []quotes.StocksFromResponse{
 	return requestToServer(arrayOfRequestData)
 }
 
-func requestToServer(arrayOfRequestData []quotes.RequestData) []quotes.StocksFromResponse{
-	stocks := make([]quotes.StocksFromResponse,0)
+func requestToServer(arrayOfRequestData []quotes.RequestData) []quotes.StocksFromResponse {
+	stocks := make([]quotes.StocksFromResponse, 0)
+
+	client := tinkoff.NewRestClient(*token)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	for _, element := range arrayOfRequestData {
-		convertJson, _ := json.Marshal(element)
-		jsonStr := []byte(convertJson)
-		req, err := http.NewRequest("POST", quotes.URL, bytes.NewBuffer(jsonStr))
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
+		instruments, err := client.SearchInstrumentByTicker(ctx, element.Ticker)
 		if err != nil {
-			panic(err)
+			log.Fatalln(err)
 		}
-		defer resp.Body.Close()
 
-		body, _ := ioutil.ReadAll(resp.Body) // []byte
-		value := gjson.Get(string(body), "payload.candles")
-
-		json.Unmarshal([]byte(value.String()), &stocks)
-		for x := range stocks {
-			stocks[x].Code = element.Ticker
+		if len(instruments) > 1 {
+			log.Fatalf("server return more one instrument %+v\n", instruments)
 		}
-		//clientDb.InsertNewQuotes(connection, stocks)
+
+		for _, instrument := range instruments {
+			fmt.Printf("{\"FIGI\":\"%+v\",\"Tiker\":\"%+v\",\"Name\":\"%+v\"},\n", instrument.FIGI, instrument.Ticker, instrument.Name)
+		}
+
+		//log.Printf("%+v\n", instruments)
+
+		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 	}
-	return stocks
-}
 
-func insertToDatabase(arrayOfRequestData []quotes.RequestData) {
-	//connection := clientDb.GetClient()
+	//instruments, err := client.SearchInstrumentByTicker(ctx, "NKE")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//
+	//if len(instruments) > 1 {
+	//	log.Fatalf("server return more one instrument %+v\n", instruments)
+	//}
+	//
+	//log.Printf("%+v\n", instruments)
+	//
+	//ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
 
 	//for _, element := range arrayOfRequestData {
 	//	convertJson, _ := json.Marshal(element)
@@ -71,12 +84,10 @@ func insertToDatabase(arrayOfRequestData []quotes.RequestData) {
 	//	body, _ := ioutil.ReadAll(resp.Body) // []byte
 	//	value := gjson.Get(string(body), "payload.candles")
 	//
-	//	stocks := make([]quotes.StocksFromResponse,0)
 	//	json.Unmarshal([]byte(value.String()), &stocks)
 	//	for x := range stocks {
 	//		stocks[x].Code = element.Ticker
 	//	}
-	//	clientDb.InsertNewQuotes(connection, stocks)
 	//}
+	return stocks
 }
-
